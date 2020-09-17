5876014490398de874ce1096b95a95928b006d96
BrizSimbir <tema_ashifin@mail.ru>
2020-09-17 14:34:58 +0300

    17.09 soap v1.1

--- WcfServiceLibrary1/ISoapBgService.cs
+++ WcfServiceLibrary1/ISoapBgService.cs
@@ -8,23 +8,24 @@ using System.Text;
 
 namespace SoapBgService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
-    [ServiceContract]//(Namespace = "http://ws.integratedService.geoserver.protei.ru/")]
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.    
+    
+    [ServiceContract(Namespace = "http://ws.integratedService.geoserver.protei.ru/")]
     public interface ISoapBgService
     {
-        [OperationContract]
+        [OperationContract(Action = "getLayers") ]//(Action = "http://ws.integratedService.geoserver.protei.ru/ISoapBgService/getLayers", Name = "getlayers")]
         List<Layer> getLayers(long userId, string username);
 
-        [OperationContract]
+        [OperationContract(Action = "getGeoObjects")]
         List<GeoObject> getGeoObjects(long userId, string username, int layerId,
                             double bottomLeftLat, double bottomLeftLon, double topRightLat, double topRightLon,
                             List<Filter> filters);
 
-        [OperationContract]
+        [OperationContract(Action = "updateLayer")]
         StatusResponse updateLayer(long systemId, long userId, string username, long layerId,
                             RequestType requestType, List<GeoObject> geoObjects);
 
-        [OperationContract]
+        [OperationContract(Action = "registerForUpdate")]
         bool registerForUpdate(long systemId, long userId, string username, int layerId,
                             double bottomLeftLat, double bottomLeftLon, double topRightLat, double topRightLon,
                             List<Filter> filters);
--- WcfServiceLibrary1/SoapBgService.cs
+++ WcfServiceLibrary1/SoapBgService.cs
@@ -13,6 +13,7 @@ using System.Web.Services.Protocols;
 namespace SoapBgService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(Namespace = "http://ws.integratedService.geoserver.protei.ru/")]
     public class SoapBgService : ISoapBgService
     {
         public List<Layer> getLayers(long userId, string username)
